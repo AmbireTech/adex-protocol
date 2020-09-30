14092919b3e8f0132898b1201870a0d7b976a2d6
Rangel Stoilov <rangelstoilov@gmail.com>
2020-09-30 14:41:11 +0300

    change header text and pass element outerHTML

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 window.onload = function () {
-  var whitepaper = this.document.getElementById("whitepaper");
   var root = this.document.getElementById("root");
-
+  var headerHTMLString = this.document.getElementById("header").outerHTML;
   document.getElementById("download").addEventListener("click", () => {
-    // var opt = {
-    //   filename: "whitepaper.pdf",
-    //   image: { type: "jpeg", quality: 0.98 },
-    //   html2canvas: { scale: 2 },
-    //   jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-    // };
-    // html2pdf().from(whitepaper).set(opt).save();
     printJS({
       printable: "root",
       type: "html",
       scanStyles: false,
       showModal: true,
-      header: `<div class="logo">
-                <h1>AdEx: A Decentralized Ad Exchange</h1>
-                <h2>Whitepaper</h2>
-                <h3>By Ivo Georgiev, Dimo Stoyanov, Vanina Ivanova</h3>
-               </div>`,
+      header: headerHTMLString,
       css: [
         "styles.css",
         "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
